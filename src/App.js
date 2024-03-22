7f03c1d9d3f9ce17155ff259e269a53ff2933ad6
athmikha <102711156+athmikha@users.noreply.github.com>
2024-03-22 22:39:36 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from  'axios';
 function App() {
+
+   async function  athmikha(){
+    console.log("athmikha");
+    const options = {
+      method: 'GET',
+      url: 'https://spotify23.p.rapidapi.com/user_profile/',
+      params: {
+        id: 'nocopyrightsounds',
+        playlistLimit: '10',
+        artistLimit: '10'
+      },
+      headers: {
+        'X-RapidAPI-Key': '89254aefd5msh3a694bb63329183p1e8451jsnf75b763a1194',
+        'X-RapidAPI-Host': 'spotify23.p.rapidapi.com'
+      }
+    };
+    
+    try {
+      const response =  await axios.request(options);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+   }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+         <button  onClick={athmikha}>athmikha</button>
     </div>
   );
 }
